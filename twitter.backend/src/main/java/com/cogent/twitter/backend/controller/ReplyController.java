d022372417d708a025fce9ecb8b777b0bbc3e1dc
Edmond Lin <edmondlinn@gmail.com>
2024-04-11 14:19:19 -0700

    apis

--- twitter.backend/src/main/java/com/cogent/twitter/backend/controller/ReplyController.java
+++ twitter.backend/src/main/java/com/cogent/twitter/backend/controller/ReplyController.java
@@ -2,6 +2,10 @@ package com.cogent.twitter.backend.controller;
 
 
 import com.cogent.twitter.backend.entity.Reply;
+import com.cogent.twitter.backend.entity.Tweet;
+import com.cogent.twitter.backend.service.ReplyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,17 +15,28 @@ import java.util.List;
 @RequestMapping("/api/v1.0/tweets")
 public class ReplyController {
 
+    @Autowired
+    private ReplyService replyService;
     // Not in spec, but needed to display tweet thread with original tweet and replies
-    @GetMapping("/replies/{tweetId}")
+    @GetMapping("/{username}/replies/{tweetId}")
     public ResponseEntity<List<Reply>> getAllReplies(
+            @PathVariable("username") String username,
             @PathVariable("tweetId") Long id) {
-        return null;
+        var data = replyService.getAllRepliesByTweet(username, id);
+        if (data == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(data, HttpStatus.CREATED);
     }
 
     @PostMapping("/{username}/reply/{tweetId}")
     public ResponseEntity<Reply> replyToTweet(
             @PathVariable("username") String username,
-            @PathVariable("tweetId") Long id) {
-        return null;
+            @PathVariable("tweetId") Long id, @RequestBody Reply reply) {
+        var data = replyService.replyToTweet(username, id, reply);
+        if (data == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(data, HttpStatus.CREATED);
     }
 }
---
+++ twitter.backend/src/main/java/com/cogent/twitter/backend/service/ReplyService.java
@@ -0,0 +1,57 @@
+package com.cogent.twitter.backend.service;
+
+import com.cogent.twitter.backend.entity.Tweet;
+import com.cogent.twitter.backend.entity.User;
+import com.cogent.twitter.backend.repository.ReplyRepository;
+import com.cogent.twitter.backend.repository.TweetRepository;
+import com.cogent.twitter.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.cogent.twitter.backend.entity.Reply;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReplyService {
+    @Autowired
+    private ReplyRepository replyRepository;
+
+    @Autowired
+    private TweetRepository tweetRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<Reply> getAllRepliesByTweet(String username, Long tweetId) {
+        Tweet tweet = tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new RuntimeException("Tweet not found"));
+        User user = userRepository.findUserByLoginId(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return replyRepository.findAllByUserAndTweet(user, tweet);
+    }
+
+
+
+    // Method to retrieve all replies for a given tweet ID
+    // Method to post a reply to a tweet
+    public Reply replyToTweet(String username, Long tweetId, Reply reply) {
+        // Retrieve the tweet using tweetId from the tweet repository
+        Tweet tweet = tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new RuntimeException("Tweet not found"));
+
+        // Retrieve the user using username from the user repository
+        User user = userRepository.findUserByLoginId(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Associate the reply with the tweet
+        reply.setTweet(tweet);
+
+        // Associate the reply with the user
+        reply.setUser(user);
+
+        // Save the reply in the database using the ReplyRepository
+        return replyRepository.save(reply);
+    }
+}
+
