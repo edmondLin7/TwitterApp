40fff530ddb2622abb1d575ec81091ceec40cfa3
Edmond Lin <edmondlinn@gmail.com>
2024-04-11 09:26:40 -0700

    logoandnavbar

--- frontend/twitter-app/src/app/app.module.ts
+++ frontend/twitter-app/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TweetComponent } from './components/tweet/tweet.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
-import { RightSidebarComponent } from './components/right-sidebar/right-sidebar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +19,11 @@ import { RegisterFormComponent } from './components/register-form/register-form.
     TweetComponent,
     LoginComponent,
     RegisterComponent,
-    LeftSidebarComponent,
-    RightSidebarComponent,
     MainContentComponent,
     LoginFormComponent,
-    RegisterFormComponent
+    RegisterFormComponent,
+    NavbarComponent,
+    LogoComponent,
   ],
   imports: [
     BrowserModule,
